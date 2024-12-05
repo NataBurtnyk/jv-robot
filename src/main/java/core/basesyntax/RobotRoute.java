0f157b32ce81a43cf7392eac12207356925a4c2a
natalia burtnyk <nataliaburtnyk@gmail.com>
2024-12-05 10:43:40 -0600

    Implemented robot navigation by x-coordinate.

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,42 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        Direction currentDirection = robot.getDirection();
+        int currentX = robot.getX();
+
+        if (toX > currentX && currentDirection != Direction.RIGHT) {
+            if(currentDirection == Direction.UP) {
+                robot.turnRight();
+            } else if(currentDirection == Direction.DOWN) {
+                robot.turnLeft();
+            } else if (currentDirection == Direction.LEFT) {
+                robot.turnLeft();
+                robot.turnLeft();
+            }
+
+            int stepsToGo = toX - currentX;
+
+            for(int i = 0; i < stepsToGo; i++) {
+                robot.stepForward();
+            }
+        }
+
+
+        if (toX < currentX && currentDirection != Direction.LEFT) {
+            if(currentDirection == Direction.UP) {
+                robot.turnLeft();
+            } else if(currentDirection == Direction.DOWN) {
+                robot.turnRight();
+            } else if (currentDirection == Direction.RIGHT) {
+                robot.turnLeft();
+                robot.turnLeft();
+            }
+            int stepsToGo = Math.abs(toX - currentX);
+
+            for(int i = 0; i < stepsToGo; i++) {
+                robot.stepForward();
+            }
+        }
+
     }
 }
